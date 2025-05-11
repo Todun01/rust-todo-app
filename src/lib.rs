221e54e629d978f8b49dd61f22637f68e213a36e
Oluwatodunni <obafemitodunni@gmail.com>
2025-05-11 15:43:03 +0100

    refactoring with lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,35 @@
+use std::process;
+pub struct Args{
+    init_command: String,
+    query: String,
+    item: Option<String>
+}
+
+impl Args {
+    pub fn parse_args(_args: &[String]) -> Result <Args, &str>{
+        if _args.len() < 3{
+            return Err("You need to enter more arguments")
+        }
+        if _args.len() == 3{
+            let init_command = _args[1].clone();
+            let query = _args[2].clone();
+            let item = None;
+            return Ok(Args{init_command, query, item});
+        }
+        let init_command = _args[1].clone();
+        let query = _args[2].clone();
+        let item = Some(_args[3].clone());
+        Ok(Args{init_command, query, item})
+
+        
+    }
+}
+
+pub fn run_app(items: &[String]){
+    let _args: Args = Args::parse_args(items).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("First argument: {}, Second argument: {}, third argument: {:?}", _args.init_command, _args.query, _args.item)
+}
