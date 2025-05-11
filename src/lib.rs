6ef757b3e4e128c688150e8086e2ee990bdea8f8
Oluwatodunni <obafemitodunni@gmail.com>
2025-05-11 17:23:48 +0100

    file creation

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use std::process;
 use std::fs;
+use std::io;
 use std::io::{Error, ErrorKind};
+use std::path::Path;
 pub struct Args{
     init_command: String,
     query: String
@@ -20,13 +22,20 @@ impl Args {
             return Err(format!("{} is not a recognized command", init_command))
         }
         let allowed_queries = vec!["add".to_string(), "show".to_string(), 
-        "remove".to_string(), "update".to_string()];
+        "remove".to_string(), "update".to_string(), "new_list".to_string()];
         if !allowed_queries.contains(&query){
             return Err(format!("{} is not a recognized command", query))
         }
         Ok(Args{init_command, query})
     }
 
+}
+pub fn new_list(name: &str) -> Result<&str, &str>{
+    fs::File::create(&format!("{}.txt",name));
+    if Path::new(name).exists(){
+        return Err("Error creating file")
+    }
+    Ok("File created successfully!")
 }
 pub fn show_list(filename: &str) -> Result<String, std::io::Error>{
     let _filetext = fs::read_to_string(filename)?;
@@ -37,14 +46,25 @@ pub fn show_list(filename: &str) -> Result<String, std::io::Error>{
     Ok(_filetext)
 }
 
+// pub fn add_item(item: &str, file_name: &str) -> Result<&str, &str>{
+
+
+//     Ok("Item added successfully!")
+// }
+
 pub fn run_app(items: &[String]){
     let _args: Args = Args::parse_args(items).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
-
-    println!("First argument: {}, Second argument: {}", 
-    _args.init_command, _args.query)
+    if _args.query == "new_list"{
+        let mut filename = String::new();
+        io::stdin().read_line(& mut filename).expect("Failed to read input");
+        let result = new_list(&filename);
+        println!("{:?}", result)
+    }
+    // println!("First argument: {}, Second argument: {}", 
+    // _args.init_command, _args.query)
 }
 
 #[cfg(test)]
@@ -58,4 +78,11 @@ fn file_testing(){
     let file_name = "/home/oluwatodunni/Desktop/testfile.txt";
     let result = show_list(file_name);
     println!("Output: {:?}", result)
+}
+
+#[test]
+fn file_creation(){
+    let file_name = "testing tings";
+    let result = new_list(file_name);
+    println!("{:?}", result)
 }
